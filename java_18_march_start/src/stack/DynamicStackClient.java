b51df2ab7f6111c2d540adf33bf55f9b9ccf5e92
Vicky Thakur <52929183+vickythakur0398@users.noreply.github.com>
2020-08-19 10:45:17 +0530

    Create DynamicStackClient.java

---
+++ java_18_march_start/src/stack/DynamicStackClient.java
@@ -0,0 +1,17 @@
+package stack;
+
+public class DyanamicStackClient {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Basic_opertaion_in_stack s = new dyanimcStack();
+		
+		s.push(10);
+		s.push(80);
+		s.push(100);
+		s.display();
+
+	}
+
+}
