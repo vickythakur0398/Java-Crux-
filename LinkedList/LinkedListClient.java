fb94f8967169cbeb9765984711b0bbe595f63310
Vicky Thakur <52929183+vickythakur0398@users.noreply.github.com>
2020-08-22 16:34:25 +0530

    client of linked list

---
+++ LinkedList/LinkedListClient.java
@@ -0,0 +1,17 @@
+package linkedList;
+
+public class Linklistclient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LinkedList list = new LinkedList();
+		
+		list.addLast(10);
+		list.addLast(20);
+		list.addLast(30);
+		list.addLast(40);
+		list.display();
+	}
+
+}
